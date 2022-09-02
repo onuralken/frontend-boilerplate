77fb530ac3f34ad19881df8e4c057fb6ed4b4e3f
Onur Alkan <onuralken@gmail.com>
2022-09-02 07:43:20 +0300

    init

---
+++ .lintstagedrc.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames.join(" ")}`;
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [buildPrettierCommand, buildEslintCommand],
+};
